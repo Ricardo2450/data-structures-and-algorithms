23b51d152ae6abb34eec95a4918dd1d97ea409fd
Ricardo Soto-Fabela <sotofabela@gmail.com>
2022-12-27 23:21:55 -0600

    added code for CC and whiteboard

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,16 +1,75 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+    def __str__(self):
+        return f"{ {self.value} }"
+
 class LinkedList:
     """
-    Put docstring here
+   A data structure that contains nodes that links/points to the next node in the list.
     """
-
     def __init__(self):
         # initialization here
-        pass
+        self.linked_list = []
+        self.head = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def __str__(self):
+        values = []
+        node = self.head
+        while node is not None:
+            values.append("{ " + str(node.value) + " }")
+            node = node.next
+        if len(values) == 0:
+            return "NULL"
+        return " -> ".join(values) + " -> NULL"
 
+    def insert(self, value):
+        new_node = Node(value, self.head)
+        self.head = new_node
 
+    def includes(self, value):
+        node = self.head
+        while node is not None:
+            if node.value == value:
+                return True
+            node = node.next
+        return False
+
+    def append(self, value):
+        self.linked_list.append(Node(value))
+
+    def insert_before(self, looking_for, value2):
+        old_list = []
+        if len(self.linked_list) > 0:
+            old_list = self.linked_list
+            self.linked_list = []
+
+        for node1 in old_list:
+            if node1.value == looking_for:
+                self.linked_list.append(Node(value2))
+            self.linked_list.append(node1)
+
+    def insert_after(self, looking_for, value2):
+        old_list = []
+        if len(self.linked_list) > 0:
+            old_list = self.linked_list
+            self.linked_list = []
+
+        for node1 in old_list:
+            self.linked_list.append(node1)
+            if node1.value == looking_for:
+                self.linked_list.append(Node(value2))
+"""
+    def to_string(self):
+        values = []
+        node = self.head
+        while node is not None:
+            values.append(str(node.value))
+            node = node.next
+        if len(values) == 0:
+            return "NULL"
+"""
 class TargetError:
     pass
---
+++ python/docs/linked_list_insertions/README.md
@@ -0,0 +1,8 @@
+# Linked list insertions
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+![](./CC-6-whiteboard.png)
+
+## Approach & Efficiency
+
